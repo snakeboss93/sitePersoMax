fc99a2afe8aa0e6ecbca114ebf8854b2e613aa82
pierreAroban <pierre.aroban@gmail.com>
2017-01-21 17:03:22 +0100

    ajout

---
+++ src/AppBundle/AppBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle principal contenant l'ensemble de l'application.
+ */
+class AppBundle extends Bundle
+{
+}
